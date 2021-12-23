45992b74e11d01cec85a3c356262b9d3ef181e15
Anna Steinhilber <annasteinhilber@gmx.de>
2021-12-23 17:06:29 +0100

    Use the correct BuildConfig

--- app/src/main/java/com/cabbagebeyond/util/FirebaseUtil.kt
+++ app/src/main/java/com/cabbagebeyond/util/FirebaseUtil.kt
@@ -3,18 +3,18 @@ package com.cabbagebeyond.util
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import com.cabbagebeyond.BuildConfig
 import com.cabbagebeyond.R
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import io.grpc.android.BuildConfig
 
 object FirebaseUtil {
 
     private const val TAG = "FirebaseUtil"
-    private const val USE_EMULATORS = BuildConfig.DEBUG
+    private var USE_EMULATORS = BuildConfig.DEBUG
 
     val firestore: FirebaseFirestore by lazy {
         return@lazy setupFirestore()
