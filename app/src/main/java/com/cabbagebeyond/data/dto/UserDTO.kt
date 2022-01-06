4695ed5f65b49cf9dad4006923a54aff2837b7d1
Anna Steinhilber <annasteinhilber@gmx.de>
2022-01-06 18:32:33 +0100

    Fix convert firestore document to user

--- app/src/main/java/com/cabbagebeyond/data/dto/UserDTO.kt
+++ app/src/main/java/com/cabbagebeyond/data/dto/UserDTO.kt
@@ -9,31 +9,31 @@ import kotlin.collections.HashMap
 
 data class UserDTO(
     @PropertyName(FIELD_NAME)
-    var name: String = "",
+    var username: String = "",
     @PropertyName(FIELD_EMAIL)
     var email: String = "",
     @PropertyName(FIELD_FEATURES)
     var features: List<String> = listOf(),
     @PropertyName(FIELD_ROLES)
-    var roles: List<String> = listOf(),
+    var roles_ids: List<String> = listOf(),
     @DocumentId
     val id: String = UUID.randomUUID().toString()
 ) {
 
     companion object {
         const val COLLECTION_TITLE = "users"
-        const val FIELD_NAME = "name"
+        const val FIELD_NAME = "username"
         const val FIELD_EMAIL = "email"
         const val FIELD_FEATURES = "features"
-        const val FIELD_ROLES = "roles"
+        const val FIELD_ROLES = "roles_ids"
     }
 
     fun toHashMap(): HashMap<String, Any> {
         return hashMapOf(
-            FIELD_NAME to name,
+            FIELD_NAME to username,
             FIELD_EMAIL to email,
             FIELD_FEATURES to features,
-            FIELD_ROLES to roles
+            FIELD_ROLES to roles_ids
         )
     }
 }
@@ -45,7 +45,7 @@ fun List<UserDTO>.asDomainModel(): List<User> {
 }
 
 fun UserDTO.asDomainModel(): User {
-    return User(name, email, features, roles, id)
+    return User(username, email, features, roles_ids, id)
 }
 
 fun List<User>.asDatabaseModel(): List<UserDTO> {
