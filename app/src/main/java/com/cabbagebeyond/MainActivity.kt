43f5b940c5e5f628218559a35a450408084c4a23
Anna Steinhilber <annasteinhilber@gmx.de>
2022-01-13 23:19:21 +0100

    Update drawer menu

--- app/src/main/java/com/cabbagebeyond/MainActivity.kt
+++ app/src/main/java/com/cabbagebeyond/MainActivity.kt
@@ -3,7 +3,6 @@ package com.cabbagebeyond
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -46,7 +45,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_admin_panel
+                R.id.nav_worlds, R.id.nav_characters, R.id.nav_admin_panel
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
