63452c5024e5b8f628ed8683292c9413e113333e
Anna Steinhilber <annasteinhilber@gmx.de>
2022-01-16 13:20:11 +0100

    Refactor CharacterDao

--- app/src/main/java/com/cabbagebeyond/data/dao/CharacterDao.kt
+++ app/src/main/java/com/cabbagebeyond/data/dao/CharacterDao.kt
@@ -4,6 +4,8 @@ import android.util.Log
 import com.cabbagebeyond.data.dto.CharacterDTO
 import com.cabbagebeyond.util.FirebaseUtil
 import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.Source
 import kotlinx.coroutines.tasks.await
 
 class CharacterDao {
@@ -14,64 +16,28 @@ class CharacterDao {
     }
 
     suspend fun getCharacters(): Result<List<CharacterDTO>> {
-        var result: Result<List<CharacterDTO>> = Result.success(mutableListOf())
-        FirebaseUtil.firestore.collection(COLLECTION_TITLE)
-            .get()
-            .addOnSuccessListener { task ->
-                val characters = task.documents.mapNotNull { documentSnapshot ->
-                    map(documentSnapshot)
-                }
-                result = Result.success(characters)
-            }
-            .addOnFailureListener { exception ->
-                result = Result.failure(exception.fillInStackTrace())
-            }
-            .await()
-        return result
+        return loadCharacters(
+            FirebaseUtil.firestore.collection(COLLECTION_TITLE)
+        )
     }
 
-    suspend fun getCharactersOrderedByName(): Result<List<CharacterDTO>> = getCharactersOrdered(CharacterDTO.FIELD_NAME)
+    suspend fun getCharactersOrderedByName(): Result<List<CharacterDTO>> =
+        getCharactersOrdered(CharacterDTO.FIELD_NAME)
 
-    suspend fun getCharactersOrderedByRace(): Result<List<CharacterDTO>> = getCharactersOrdered(CharacterDTO.FIELD_RACE)
+    suspend fun getCharactersOrderedByRace(): Result<List<CharacterDTO>> =
+        getCharactersOrdered(CharacterDTO.FIELD_RACE)
 
-    suspend fun getCharactersOrderedByType(): Result<List<CharacterDTO>> = getCharactersOrdered(CharacterDTO.FIELD_TYPE)
+    suspend fun getCharactersOrderedByType(): Result<List<CharacterDTO>> =
+        getCharactersOrdered(CharacterDTO.FIELD_TYPE)
 
-    suspend fun getCharactersOrderedByWorld(): Result<List<CharacterDTO>> = getCharactersOrdered(CharacterDTO.FIELD_WORLD)
-
-    private suspend fun getCharactersOrdered(fieldName: String): Result<List<CharacterDTO>> {
-        var result: Result<List<CharacterDTO>> = Result.success(mutableListOf())
-        FirebaseUtil.firestore.collection(COLLECTION_TITLE)
-            .orderBy(fieldName)
-            .get(Source.CACHE)
-            .addOnSuccessListener { task ->
-                val characters = task.documents.mapNotNull { documentSnapshot ->
-                    map(documentSnapshot)
-                }
-                result = Result.success(characters)
-            }
-            .addOnFailureListener { exception ->
-                result = Result.failure(exception.fillInStackTrace())
-            }
-            .await()
-        return result
-    }
+    suspend fun getCharactersOrderedByWorld(): Result<List<CharacterDTO>> =
+        getCharactersOrdered(CharacterDTO.FIELD_WORLD)
 
     suspend fun getCharactersOfUser(id: String): Result<List<CharacterDTO>> {
-        var result: Result<List<CharacterDTO>> = Result.success(mutableListOf())
-        FirebaseUtil.firestore.collection(COLLECTION_TITLE)
-            .whereEqualTo(CharacterDTO.FIELD_OWNER, id)
-            .get()
-            .addOnSuccessListener { task ->
-                val characters = task.documents.mapNotNull { documentSnapshot ->
-                    map(documentSnapshot)
-                }
-                result = Result.success(characters)
-            }
-            .addOnFailureListener { exception ->
-                result = Result.failure(exception.fillInStackTrace())
-            }
-            .await()
-        return result
+        return loadCharacters(
+            FirebaseUtil.firestore.collection(COLLECTION_TITLE)
+                .whereEqualTo(CharacterDTO.FIELD_OWNER, id)
+        )
     }
 
     suspend fun getCharacter(id: String): Result<CharacterDTO> {
@@ -108,32 +74,61 @@ class CharacterDao {
             .addOnFailureListener { e -> Log.w(TAG, "Error deleting document", e) }
     }
 
+
+    private suspend fun loadCharacters(
+        query: Query,
+        source: Source = Source.DEFAULT
+    ): Result<List<CharacterDTO>> {
+        var result: Result<List<CharacterDTO>> = Result.success(mutableListOf())
+        query
+            .get(source)
+            .addOnSuccessListener { task ->
+                val characters = task.documents.mapNotNull { documentSnapshot ->
+                    map(documentSnapshot)
+                }
+                result = Result.success(characters)
+            }
+            .addOnFailureListener { exception ->
+                result = Result.failure(exception.fillInStackTrace())
+            }
+            .await()
+        return result
+    }
+
+    private suspend fun getCharactersOrdered(fieldName: String): Result<List<CharacterDTO>> {
+        return loadCharacters(
+            FirebaseUtil.firestore.collection(COLLECTION_TITLE)
+                .orderBy(fieldName),
+            Source.CACHE
+        )
+    }
+
     private fun map(documentSnapshot: DocumentSnapshot): CharacterDTO {
         return CharacterDTO(
-        documentSnapshot.get(CharacterDTO.FIELD_NAME, String::class.java) ?: "",
+            documentSnapshot.get(CharacterDTO.FIELD_NAME, String::class.java) ?: "",
             documentSnapshot.get(CharacterDTO.FIELD_RACE, String::class.java) ?: "",
-        documentSnapshot.get(CharacterDTO.FIELD_DESCRIPTION, String::class.java) ?: "",
-        documentSnapshot.get(CharacterDTO.FIELD_CHARISMA, Int::class.java) ?: 0,
-        documentSnapshot.get(CharacterDTO.FIELD_CONSTITUTION, String::class.java) ?: "",
-        documentSnapshot.get(CharacterDTO.FIELD_DECEPTION, String::class.java) ?: "",
-        documentSnapshot.get(CharacterDTO.FIELD_DEXTERITY, String::class.java) ?: "",
-        documentSnapshot.get(CharacterDTO.FIELD_INTELLIGENCE, String::class.java) ?: "",
-        documentSnapshot.get(CharacterDTO.FIELD_INVESTIGATION, String::class.java) ?: "",
-        documentSnapshot.get(CharacterDTO.FIELD_PERCEPTION, String::class.java) ?: "",
-        documentSnapshot.get(CharacterDTO.FIELD_STEALTH, String::class.java) ?: "",
-        documentSnapshot.get(CharacterDTO.FIELD_STRENGTH, String::class.java) ?: "",
-        documentSnapshot.get(CharacterDTO.FIELD_WILLPOWER, String::class.java) ?: "",
-        documentSnapshot.get(CharacterDTO.FIELD_MOVEMENT, Int::class.java) ?: 0,
-        documentSnapshot.get(CharacterDTO.FIELD_PARRY, Int::class.java) ?: 0,
-        documentSnapshot.get(CharacterDTO.FIELD_TOUGHNESS, String::class.java) ?: "",
-        documentSnapshot.get(CharacterDTO.FIELD_ABILITIES) as List<String>,
-        documentSnapshot.get(CharacterDTO.FIELD_EQUIPMENTS) as List<String>,
-        documentSnapshot.get(CharacterDTO.FIELD_FORCES) as List<String>,
-        documentSnapshot.get(CharacterDTO.FIELD_HANDICAPS) as List<String>,
-        documentSnapshot.get(CharacterDTO.FIELD_TALENTS) as List<String>,
-        documentSnapshot.get(CharacterDTO.FIELD_TYPE, String::class.java) ?: "",
-        documentSnapshot.get(CharacterDTO.FIELD_OWNER, String::class.java) ?: "",
-        documentSnapshot.get(CharacterDTO.FIELD_WORLD, String::class.java) ?: "",
+            documentSnapshot.get(CharacterDTO.FIELD_DESCRIPTION, String::class.java) ?: "",
+            documentSnapshot.get(CharacterDTO.FIELD_CHARISMA, Int::class.java) ?: 0,
+            documentSnapshot.get(CharacterDTO.FIELD_CONSTITUTION, String::class.java) ?: "",
+            documentSnapshot.get(CharacterDTO.FIELD_DECEPTION, String::class.java) ?: "",
+            documentSnapshot.get(CharacterDTO.FIELD_DEXTERITY, String::class.java) ?: "",
+            documentSnapshot.get(CharacterDTO.FIELD_INTELLIGENCE, String::class.java) ?: "",
+            documentSnapshot.get(CharacterDTO.FIELD_INVESTIGATION, String::class.java) ?: "",
+            documentSnapshot.get(CharacterDTO.FIELD_PERCEPTION, String::class.java) ?: "",
+            documentSnapshot.get(CharacterDTO.FIELD_STEALTH, String::class.java) ?: "",
+            documentSnapshot.get(CharacterDTO.FIELD_STRENGTH, String::class.java) ?: "",
+            documentSnapshot.get(CharacterDTO.FIELD_WILLPOWER, String::class.java) ?: "",
+            documentSnapshot.get(CharacterDTO.FIELD_MOVEMENT, Int::class.java) ?: 0,
+            documentSnapshot.get(CharacterDTO.FIELD_PARRY, Int::class.java) ?: 0,
+            documentSnapshot.get(CharacterDTO.FIELD_TOUGHNESS, String::class.java) ?: "",
+            documentSnapshot.get(CharacterDTO.FIELD_ABILITIES) as List<String>,
+            documentSnapshot.get(CharacterDTO.FIELD_EQUIPMENTS) as List<String>,
+            documentSnapshot.get(CharacterDTO.FIELD_FORCES) as List<String>,
+            documentSnapshot.get(CharacterDTO.FIELD_HANDICAPS) as List<String>,
+            documentSnapshot.get(CharacterDTO.FIELD_TALENTS) as List<String>,
+            documentSnapshot.get(CharacterDTO.FIELD_TYPE, String::class.java) ?: "",
+            documentSnapshot.get(CharacterDTO.FIELD_OWNER, String::class.java) ?: "",
+            documentSnapshot.get(CharacterDTO.FIELD_WORLD, String::class.java) ?: "",
             documentSnapshot.id
         )
     }
