ece65f579c68dbe2c3dae8535243473dccc7b811
Anna Steinhilber <annasteinhilber@gmx.de>
2022-01-20 18:22:44 +0100

    WIP: Ability details screen

--- app/src/main/java/com/cabbagebeyond/ui/collection/abilities/details/AbilityDetailsFragment.kt
+++ app/src/main/java/com/cabbagebeyond/ui/collection/abilities/details/AbilityDetailsFragment.kt
@@ -5,10 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.lifecycle.Observer
 import com.cabbagebeyond.data.AbilityDataSource
+import com.cabbagebeyond.data.WorldDataSource
 import com.cabbagebeyond.databinding.AbilityDetailsFragmentBinding
 import com.cabbagebeyond.model.User
+import com.cabbagebeyond.model.World
 import com.cabbagebeyond.util.Feature
 import org.koin.android.ext.android.inject
 
@@ -32,7 +37,8 @@ class AbilityDetailsFragment : Fragment() {
         val ability = AbilityDetailsFragmentArgs.fromBundle(requireArguments()).ability
 
         val dataSource: AbilityDataSource by inject()
-        _viewModel = AbilityDetailsViewModel(ability, dataSource, User("", "", listOf(Feature.CONFIGURE_APP.name), listOf(), ""))
+        val worldDataSource: WorldDataSource by inject()
+        _viewModel = AbilityDetailsViewModel(ability, dataSource, worldDataSource, User("", "", listOf(Feature.CONFIGURE_APP.name), listOf(), ""), requireContext())
 
         _binding.viewModel = _viewModel
         _binding.ability = _viewModel.ability.value
@@ -44,15 +50,80 @@ class AbilityDetailsFragment : Fragment() {
         })
 
         _viewModel.isEditing.observe(viewLifecycleOwner, Observer {
-            it?.let {
-                _binding.editName.isEnabled = it
-                _binding.attributeSpinner.isEnabled = it
-                _binding.worldSpinner.isEnabled = it
-                _binding.description.isEnabled = it
+            it?.let { isEditing ->
+                if (isEditing) {
+                    _binding.readGroup.visibility = View.INVISIBLE
+                    _binding.editGroup.visibility = View.VISIBLE
+                } else {
+                    _binding.readGroup.visibility = View.VISIBLE
+                    _binding.editGroup.visibility = View.INVISIBLE
+                }
             }
+        })
+
+        _viewModel.attributes.observe(viewLifecycleOwner, Observer {
+            val list = it ?: listOf()
+            setupAttributeSpinner(ability.attribute, list)
+        })
+
+        _viewModel.worlds.observe(viewLifecycleOwner, Observer {
+            val list = it ?: listOf()
+            setupWorldSpinner(ability.world, list)
         })
 
         return _binding.root
     }
 
+    private fun setupAttributeSpinner(attribute: String, attributes: List<String>) {
+        setupSpinner(attribute, attributes, _binding.attributeSpinner, object :
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parentView: AdapterView<*>?,
+                selectedItemView: View?,
+                position: Int,
+                id: Long
+            ) {
+                val item = attributes[position]
+                _viewModel.onAttributeSelected(item)
+            }
+
+            override fun onNothingSelected(parentView: AdapterView<*>?) {
+                // your code here
+            }
+        })
+    }
+
+    private fun setupWorldSpinner(world: String?, worlds: List<World>) {
+        setupSpinner(world, worlds, _binding.worldSpinner, object :
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parentView: AdapterView<*>?,
+                selectedItemView: View?,
+                position: Int,
+                id: Long
+            ) {
+                val item = worlds[position]
+                _viewModel.onWorldSelected(item)
+            }
+
+            override fun onNothingSelected(parentView: AdapterView<*>?) {
+                // your code here
+            }
+        })
+    }
+
+    private fun setupSpinner(selectedItem: Any?, items: List<Any?>, spinner: Spinner, listener: AdapterView.OnItemSelectedListener) {
+        ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_spinner_item,
+            items
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+        val position = items.indexOf(selectedItem)
+        spinner.setSelection(position)
+
+        spinner.onItemSelectedListener = listener
+    }
 }
--- app/src/main/java/com/cabbagebeyond/ui/collection/abilities/details/AbilityDetailsViewModel.kt
+++ app/src/main/java/com/cabbagebeyond/ui/collection/abilities/details/AbilityDetailsViewModel.kt
@@ -1,17 +1,26 @@
 package com.cabbagebeyond.ui.collection.abilities.details
 
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.cabbagebeyond.R
 import com.cabbagebeyond.data.AbilityDataSource
+import com.cabbagebeyond.data.WorldDataSource
 import com.cabbagebeyond.model.Ability
 import com.cabbagebeyond.model.User
+import com.cabbagebeyond.model.World
 import com.cabbagebeyond.util.Feature
 import kotlinx.coroutines.launch
 
-class AbilityDetailsViewModel(private val _ability: Ability, private val _abilityDataSource: AbilityDataSource, private val user: User) : ViewModel() {
+class AbilityDetailsViewModel(
+    givenAbility: Ability,
+    private val _abilityDataSource: AbilityDataSource,
+    private val _worldDataSource: WorldDataSource,
+    user: User,
+    context: Context
+) : ViewModel() {
 
     val userCanEdit = user.features.contains(Feature.CONFIGURE_APP.name)
 
@@ -19,16 +28,24 @@ class AbilityDetailsViewModel(private val _ability: Ability, private val _abilit
     val isEditing: LiveData<Boolean>
         get() = _isEditing
 
-    var ability = MutableLiveData(_ability)
+    var ability = MutableLiveData(givenAbility)
 
-    private var _fabImage = MutableLiveData<Int>(R.drawable.ic_edit)
+    private var _fabImage = MutableLiveData(R.drawable.ic_edit)
     val fabImage: LiveData<Int>
         get() = _fabImage
 
+    private var _worlds = MutableLiveData<List<World>>()
+    val worlds: LiveData<List<World>>
+        get() = _worlds
 
+    private var _attributes = MutableLiveData<List<String>>()
+    val attributes: LiveData<List<String>>
+        get() = _attributes
 
     init {
-
+        // for MVP the attributes are stored in resources.
+        val stringArray = context.resources.getStringArray(R.array.attributes)
+        _attributes.value = stringArray.toList()
     }
 
     fun onClickFab() {
@@ -43,7 +60,23 @@ class AbilityDetailsViewModel(private val _ability: Ability, private val _abilit
     }
 
     private fun onEdit() {
-        _fabImage.value = R.drawable.ic_add
+        _fabImage.value = R.drawable.ic_save
+        if (_worlds.value == null) {
+            loadWorlds()
+        }
+        if (_attributes.value == null) {
+            loadAttributes()
+        }
+    }
+
+    private fun loadWorlds() {
+        viewModelScope.launch {
+            _worlds.value = _worldDataSource.getWorlds().getOrDefault(listOf())
+        }
+    }
+
+    private fun loadAttributes() {
+
     }
 
     private fun onSave() {
@@ -57,5 +90,17 @@ class AbilityDetailsViewModel(private val _ability: Ability, private val _abilit
         viewModelScope.launch {
             _abilityDataSource.saveAbility(ability)
         }
+    }
+
+    fun onAttributeSelected(attribute: String?) {
+        val givenAbility = ability.value
+        givenAbility?.attribute = attribute ?: ""
+        ability.value = givenAbility
+    }
+
+    fun onWorldSelected(world: World?) {
+        val givenAbility = ability.value
+        givenAbility?.world = world?.name ?: ""
+        ability.value = givenAbility
     }
 }
