790f2a2c35790873306fe787d7d8b52be3d4c4f8
Anna Steinhilber <annasteinhilber@gmx.de>
2022-03-23 22:45:10 +0100

    Create motion for buttons

--- app/src/main/java/com/cabbagebeyond/ui/onboarding/OnboardingFragment.kt
+++ app/src/main/java/com/cabbagebeyond/ui/onboarding/OnboardingFragment.kt
@@ -1,11 +1,15 @@
 package com.cabbagebeyond.ui.onboarding
 
+import android.app.AlertDialog
+import android.app.Dialog
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
+import com.cabbagebeyond.R
 import com.cabbagebeyond.databinding.FragmentOnboardingBinding
 
 class OnboardingFragment : Fragment() {
@@ -27,7 +31,51 @@ class OnboardingFragment : Fragment() {
         _binding = FragmentOnboardingBinding.inflate(inflater)
         _binding.viewpager.adapter = _adapter
 
+        _binding.viewpager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+                val progress = (position + positionOffset) / (_viewModel.items.size - 1)
+                _binding.root.progress = progress
+                updateTextOfNextButton(progress)
+            }
+        })
+
+        _binding.buttonSkip.setOnClickListener {
+            finishOnboarding()
+        }
+
+        _binding.buttonNext.setOnClickListener {
+            if (_binding.root.progress == 1f) {
+                finishOnboarding()
+            } else {
+                scrollToNextPage()
+            }
+        }
+
+        _binding.buttonPrevious.setOnClickListener {
+            scrollToPreviousPage()
+        }
+
         return _binding.root
     }
 
+    private fun updateTextOfNextButton(progress: Float) {
+        _binding.buttonNext.text = if (progress == 1f) {
+            resources.getString(R.string.button_finish)
+        } else {
+            resources.getString(R.string.button_next)
+        }
+    }
+
+    private fun scrollToNextPage() {
+        _binding.viewpager.currentItem += 1
+    }
+
+    private fun scrollToPreviousPage() {
+        _binding.viewpager.currentItem -= 1
+    }
+
+    private fun finishOnboarding() {
+        AlertDialog.Builder(context).setPositiveButton("OK") { _, _ -> }
+            .show()
+    }
 }
