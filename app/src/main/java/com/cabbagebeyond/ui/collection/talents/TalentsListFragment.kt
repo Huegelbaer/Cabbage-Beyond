ac44f4288b8f36a82028dfa4c52a80e92f1a735e
Anna Steinhilber <annasteinhilber@gmx.de>
2022-05-08 10:44:50 +0200

    WIP: Talent filter

--- app/src/main/java/com/cabbagebeyond/ui/collection/talents/TalentsListFragment.kt
+++ app/src/main/java/com/cabbagebeyond/ui/collection/talents/TalentsListFragment.kt
@@ -1,23 +1,26 @@
 package com.cabbagebeyond.ui.collection.talents
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.navigation.fragment.findNavController
+import com.cabbagebeyond.R
 import com.cabbagebeyond.data.TalentDataSource
 import com.cabbagebeyond.databinding.FragmentTalentsListBinding
 import com.cabbagebeyond.model.Talent
+import com.cabbagebeyond.model.World
+import com.cabbagebeyond.ui.collection.CollectionListFragment
+import com.cabbagebeyond.ui.collection.CollectionListViewModel
+import com.google.android.material.chip.ChipGroup
 import org.koin.android.ext.android.inject
 
-class TalentsListFragment : Fragment() {
+class TalentsListFragment : CollectionListFragment() {
 
-    private val _viewModel: TalentsViewModel by lazy {
-        val dataSource: TalentDataSource by inject()
-        TalentsViewModel(dataSource)
-    }
+    private val _viewModel: TalentsViewModel
+        get() = viewModel as TalentsViewModel
 
     private lateinit var _binding: FragmentTalentsListBinding
     private lateinit var _adapter: TalentsRecyclerViewAdapter
@@ -27,6 +30,9 @@ class TalentsListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentTalentsListBinding.inflate(inflater)
+
+        val dataSource: TalentDataSource by inject()
+        viewModel = TalentsViewModel(requireActivity().application, dataSource)
 
         val clickListener = TalentClickListener {
             _viewModel.onTalentClicked(it)
@@ -50,7 +56,43 @@ class TalentsListFragment : Fragment() {
             }
         }
 
+        _viewModel.interaction.observe(viewLifecycleOwner) {
+            it?.let {
+                when (it) {
+                    is TalentsViewModel.Interaction.OpenFilter -> {
+                        showFilterDialog(it.types, it.ranks, it.worlds)
+                    }
+                }
+                _viewModel.onInteractionCompleted()
+            }
+        }
+
+        setHasOptionsMenu(true)
+
         return _binding.root
+    }
+
+    private fun showFilterDialog(types: CollectionListViewModel.FilterData<TalentType>, ranks: CollectionListViewModel.FilterData<TalentRank>, worlds: CollectionListViewModel.FilterData<World>) {
+        val viewInflated = layoutInflater.inflate(R.layout.content_view_talents_filter, null)
+        val typesChipGroup = viewInflated.findViewById<ChipGroup>(R.id.filter_type_chip_group)
+        val ranksChipGroup = viewInflated.findViewById<ChipGroup>(R.id.filter_rank_chip_group)
+        val worldsChipGroup = viewInflated.findViewById<ChipGroup>(R.id.filter_world_chip_group)
+
+        prepareChipGroup(typesChipGroup, types)
+        prepareChipGroup(ranksChipGroup, ranks)
+        prepareChipGroup(worldsChipGroup, worlds)
+
+        AlertDialog.Builder(requireContext())
+            .setTitle(R.string.menu_filter)
+            .setView(viewInflated)
+            .setPositiveButton(R.string.menu_filter) { _, _ ->
+                val selectedType = types.values.getOrNull(typesChipGroup.checkedChipId)
+                val selectedRank = ranks.values.getOrNull(typesChipGroup.checkedChipId)
+                val selectedWorld = worlds.values.getOrNull(worldsChipGroup.checkedChipId)
+                _viewModel.filter(selectedType, selectedRank, selectedWorld)
+            }
+            .setNegativeButton(R.string.dialog_button_cancel, null)
+            .show()
     }
 
     private fun showDetails(talent: Talent) {
--- app/src/main/java/com/cabbagebeyond/ui/collection/talents/TalentsViewModel.kt
+++ app/src/main/java/com/cabbagebeyond/ui/collection/talents/TalentsViewModel.kt
@@ -1,17 +1,31 @@
 package com.cabbagebeyond.ui.collection.talents
 
+import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.cabbagebeyond.data.TalentDataSource
 import com.cabbagebeyond.model.Talent
+import com.cabbagebeyond.model.World
+import com.cabbagebeyond.ui.collection.CollectionListViewModel
 import kotlinx.coroutines.launch
 
 class TalentsViewModel(
+    private val app: Application,
     private val talentDataSource: TalentDataSource
-) : ViewModel() {
+) : CollectionListViewModel(app) {
 
+    object Filter {
+        var selectedType: TalentType? = null
+        var selectedRank: TalentRank? = null
+        var selectedWorld: World? = null
+    }
+
+    sealed class Interaction {
+        data class OpenFilter(val types: FilterData<TalentType>, val ranks: FilterData<TalentRank>, val worlds: FilterData<World>) : Interaction()
+    }
+
+    private var _talents = listOf<Talent>()
     private var _items = MutableLiveData<List<Talent>>()
     val items: LiveData<List<Talent>>
         get() = _items
@@ -20,9 +34,16 @@ class TalentsViewModel(
     val selectedTalent: LiveData<Talent?>
         get() = _selectedTalent
 
+    private var _interaction = MutableLiveData<Interaction?>()
+    val interaction: LiveData<Interaction?>
+        get() = _interaction
+
+    private var _activeFilter = Filter
+
     init {
         viewModelScope.launch {
-            _items.value = talentDataSource.getTalents().getOrDefault(listOf())
+            _talents = talentDataSource.getTalents().getOrDefault(listOf())
+            _items.value = _talents
         }
     }
 
@@ -32,5 +53,43 @@ class TalentsViewModel(
 
     fun onNavigationCompleted() {
         _selectedTalent.value = null
+    }
+
+    override fun onSelectFilter() {
+        val types = _talents.mapNotNull { talent -> talent.type?.let { TalentType.create(it, app) } }.toSet().toList()
+        val ranks = _talents.mapNotNull { talent -> talent.rangRequirement?.let { TalentRank.create(it, app) } }.toSet().toList()
+        val worlds = _talents.mapNotNull { it.world }.toSet().toList()
+
+        _interaction.value = Interaction.OpenFilter(
+            FilterData(types, _activeFilter.selectedType, TalentType::title),
+            FilterData(ranks, _activeFilter.selectedRank, TalentRank::title),
+            FilterData(worlds, _activeFilter.selectedWorld, World::name)
+        )
+    }
+
+    fun filter(type: TalentType?, rank: TalentRank?, world: World?) {
+        _activeFilter.selectedType = type
+        _activeFilter.selectedRank = rank
+        _activeFilter.selectedWorld = world
+
+        viewModelScope.launch {
+            _items.value = _talents.filter { talent ->
+                val iType = type?.let { type ->
+                    talent.type == type.type
+                } ?: true
+                val iRank = rank?.let { rank ->
+                    talent.rangRequirement == rank.rank
+                } ?: true
+                val iWorld = world?.let { world ->
+                    talent.world == world
+                } ?: true
+
+                iType && iRank && iWorld
+            }
+        }
+    }
+
+    fun onInteractionCompleted() {
+        _interaction.value = null
     }
 }
